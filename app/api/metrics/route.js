e09a4057714f1756aa5d956fde437cde2238f304
RedzoEbad <ebadkhan6292002@gmail.com>
2026-05-10 19:01:10 +0500

    Metrics added

---
+++ app/api/metrics/route.js
@@ -0,0 +1,15 @@
+import { NextResponse } from 'next/server';
+import client from 'prom-client';
+
+// Initializing default metrics
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics();
+
+export async function GET() {
+  const metrics = await client.register.metrics();
+  return new NextResponse(metrics, {
+    headers: {
+      'Content-Type': client.register.contentType,
+    },
+  });
+}
